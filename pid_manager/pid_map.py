443d3428faaaca1cc5d6a0fbfdde8da5eca91d30
yassminat <yasminekamel1@gmail.com>
2021-03-03 20:41:28 -0500

    created unit testing file, wrote test results to test_log_file, updated md file

--- pid_manager/pid_map.py
+++ pid_manager/pid_map.py
@@ -2,12 +2,19 @@ MIN_PID = 300
 MAX_PID = 5000
 
 
-class PID_map():
+class PIDMap():
+    min_pid = None
+    max_pid = None
+    available_pids = None
+    pids = None
+
     def __init__(self, min_pid=MIN_PID, max_pid=MAX_PID):
         self.min_pid = min_pid
         self.max_pid = max_pid
         self.available_pids = []  # To improve time complexity of searching for availalb_pid
-        self.allocate_map  # create pid_map upon instantiation
+        self.pids = {}
+        self.allocate_map()  # create pids map upon instantiation
+        # print("allocate result", x)
 
     def allocate_map(self):
         ''' allocate_map takes no argumens and creates a PID dictionary with keys 
@@ -16,9 +23,9 @@ class PID_map():
         try:
             # Create dictionary with keys in range min_pid through max pid+1, initial values 0
             pid_range = [*range(self.min_pid, self.max_pid+1)]
-            pid_map = dict.fromkeys(pid_range, 0)
+            self.pids = dict.fromkeys(pid_range, 0)
             # All PIDs will be initial available
-            available_pids = pid_range
+            self.available_pids = pid_range
             return 1
         except:
             return -1
@@ -26,26 +33,26 @@ class PID_map():
     def allocate_pid(self):
         '''
         allocate_pid gets an available PID from available_pids list, removes it from list,
-        and updates value for PID in pid_map dictionary to 1.
+        and updates value for PID in pids map dictionary to 1.
         '''
         try:
             # Get first available PID and remove it from list
             pid = self.available_pids.pop(0)
-            self.pid_map[pid] = 1  # Update value in map
-            return 1  # success
+            self.pids[pid] = 1  # Update value in map
+            return (pid, 1)  # success
         except:
             return -1
 
     def release_pid(self, pid):
         '''
-        release_pid accepts a PID to be released, updates the value in the pid_map for that pid to 0, appends the pid to available_pids list. 
+        release_pid accepts a PID to be released, updates the value in the pids map for that pid to 0, appends the pid to available_pids list. 
         '''
         try:
             # Check if pid is already available
-            if self.pid_map[pid] == 0:
+            if self.pids[pid] == 0:
                 return 1
             # releas_pid otherwise
-            self.pid_map[pid] = 0
+            self.pids[pid] = 0
             self.available_pids.append(pid)
             return 1
         except:
---
+++ pid_manager/pid_map_tests.py
@@ -0,0 +1,65 @@
+import unittest
+from pid_map import PIDMap
+
+
+class PIDMapTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.pid_map = PIDMap()
+        self.output_file = open('test_log_file.txt', 'a')
+
+    def test_pid_min(self):
+        self.output_file.write("Testing min PID value equals 300... ")
+        self.assertEqual(self.pid_map.min_pid, 300)
+
+    def test_pid_max(self):
+        self.output_file.write("Testing max PID value equals 5000... ")
+        self.assertEqual(self.pid_map.max_pid, 5000)
+
+    def test_pid_map(self):
+        self.output_file.write(
+            "Testing to ensure all values from 300 to 5000 are in PID Map... ")
+        for i in range(300, 5001):
+            self.assertTrue(i in self.pid_map.pids)
+
+    def test_pid_map_values(self):
+        self.output_file.write(
+            "Testing to ensure that all PIDs are initially set to 0, i.e. available... ")
+        # if successful, non of the PIDs in the map will have value 1
+        self.assertFalse(1 in self.pid_map.pids.values())
+
+    def test_allocate_pid(self):
+        self.output_file.write("Testing PID allocation: ")
+        x = self.pid_map.allocate_pid()
+        if x != -1:
+            self.output_file.write("Allocate Map returned 1, for successful.")
+            # if successful one of the items in the PIDS map will have 1 as a value
+            # and the pid will no longer be in the available_pids list
+            self.output_file.write(
+                "Testing that allocation cause update in PID Map and available PIDs list... ")
+            my_key = x[0]
+            self.output_file.write(f"Allocated PID: {my_key}")
+            self.assertTrue(self.pid_map.pids[my_key] == 1 and
+                            my_key not in self.pid_map.available_pids)
+
+    def test_release_pid(self):
+        self.output_file.write("Testing release_pid... ")
+        x = self.pid_map.allocate_pid()
+        if x != -1:
+            my_key = x[0]
+            self.output_file.write(
+                f'Allocated PID {my_key}, releasing this PID...')
+            y = self.pid_map.release_pid(my_key)
+            if y != -1:
+                self.output_file.write(
+                    "Method release_pid returned 1 for success... \nTesting to ensure PID value is updated to 0 and PID is added to available_pids... ")
+                # if successful my_key should have value of 0 in pids and should be in available pids list
+                self.assertTrue(self.pid_map.pids[my_key] == 0
+                                and my_key in self.pid_map.available_pids)
+
+
+if __name__ == '__main__':
+    log_file = 'test_log_file.txt'
+    with open(log_file, "a") as f:
+        runner = unittest.TextTestRunner(f)
+        unittest.main(testRunner=runner)
